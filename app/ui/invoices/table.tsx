792efa43affbd95a6b3d656645fb964cdc48ce2d
jribunivesp <2101617@aluno.univesp.br>
2023-11-29 00:20:03 -0300

    de novo

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -90,7 +90,7 @@ export default async function InvoicesTable({
                         className="rounded-full"
                         width={28}
                         height={28}
-                        alt='rounded-full'
+                        alt="Screenshots of the dashboard project showing mobile version"
                       />
                       <p>{invoice.name}</p>
                     </div>
